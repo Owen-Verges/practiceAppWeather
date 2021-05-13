1d7cff50f2ac16cb67529bb38db0ad043d97df2d
owen_verges <owen.verges12321@gmail.com>
2021-05-13 14:27:33 -0500

    [feat] rebuilt form ground up from memory

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const express = require('express');
-const path  = require('path');
+const path = require('path');
 const {weather} = require('./fakeData');
-const app = express();
+const express = require('express')
 
-app.use(express.static(path.resolve(__dirname, './client/dist')))
-app.get('/', (req, res) => {
-  res.end();
-})
-app.get('/data', (req, res) => {
-  console.log(weather);
-  res.send(weather)
-})
-app.listen(8080, () => console.log('app listening on localhost:8080'))
+const app = express()
+
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '/client/dist')))
+app.get('/', (req, res) => res.end())
+app.get('/data', (req, res) => res.send(weather))
+app.listen(8080, () => console.log('server running at localhost:8080'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,17 @@
-const path = require('path');
+const path = require('path')
 
-const SRC_DIR = path.join(__dirname, '/client/src')
-const DIST_DIR = path.join(__dirname, '/client/dist') 
 module.exports = {
-  devtool: 'eval-source-map', 
-  entry: `${SRC_DIR}/index.jsx`,
+  devtool: "eval-source-map",
+  entry: `${path.join(__dirname, './client/src')}/index.jsx`,
   output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
+    filename: "bundle.js",
+    path: `${path.join(__dirname, './client/dist')}`
   },
   module: {
-    rules: [
-      {
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
+    rules: [{
+      test: /\.jsx$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+    }]
   }
-};
+}
